b72f6fd19b0e44e80f86dac8861407b0bbfe5c22
Adrian Mejia <axm7988@rit.edu>
2012-05-09 22:39:51 -0400

    jasmine bdd added and test cases

--- web.rb
+++ web.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
 
+get '/tz/:file' do
+  t = File.read("public/tz/#{params[:file]}")
+  t.gsub!(/^#.*\n/, '')
+  t.gsub!(/^\n/, '')
+  t
+end
+
 get '/' do
    #redirect '/index.html'
    File.new('public/index.html').readlines
